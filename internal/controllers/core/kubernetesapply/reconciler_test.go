130fcdc53858bd0a4fbcdd722c58c18a557d3eeb
Lizz Thabet <lizzthabet@gmail.com>
2021-10-25 10:51:43 -0400

    controllers: add `DisableSpec` and `DisableStatus` behavior to `KubernetesApply`

--- internal/controllers/core/kubernetesapply/reconciler_test.go
+++ internal/controllers/core/kubernetesapply/reconciler_test.go
@@ -1,8 +1,8 @@
 package kubernetesapply
 
 import (
-	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 
@@ -11,6 +11,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/tilt-dev/tilt/internal/build"
 	"github.com/tilt-dev/tilt/internal/controllers/fake"
@@ -23,6 +24,10 @@ import (
 	"github.com/tilt-dev/tilt/pkg/apis"
 	"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1"
 )
+
+// Test constants
+const timeout = time.Second * 10
+const interval = 5 * time.Millisecond
 
 func TestImageIndexing(t *testing.T) {
 	f := newFixture(t)
@@ -39,7 +44,7 @@ func TestImageIndexing(t *testing.T) {
 	// Verify we can index one image map.
 	reqs := f.r.indexer.Enqueue(&v1alpha1.ImageMap{ObjectMeta: metav1.ObjectMeta{Name: "image-a"}})
 	assert.ElementsMatch(t, []reconcile.Request{
-		reconcile.Request{NamespacedName: types.NamespacedName{Name: "a"}},
+		{NamespacedName: types.NamespacedName{Name: "a"}},
 	}, reqs)
 
 	kb := v1alpha1.KubernetesApply{
@@ -55,17 +60,21 @@ func TestImageIndexing(t *testing.T) {
 	// Verify we can index one image map to two applies.
 	reqs = f.r.indexer.Enqueue(&v1alpha1.ImageMap{ObjectMeta: metav1.ObjectMeta{Name: "image-c"}})
 	assert.ElementsMatch(t, []reconcile.Request{
-		reconcile.Request{NamespacedName: types.NamespacedName{Name: "a"}},
-		reconcile.Request{NamespacedName: types.NamespacedName{Name: "b"}},
+		{NamespacedName: types.NamespacedName{Name: "a"}},
+		{NamespacedName: types.NamespacedName{Name: "b"}},
 	}, reqs)
 
+	// Get the latest ka, since resource version numbers
+	// may have changed since its creation and mismatched
+	// versions will throw an error on update
+	f.MustGet(types.NamespacedName{Name: "a"}, &ka)
 	ka.Spec.ImageMaps = []string{"image-a"}
 	f.Update(&ka)
 
 	// Verify we can remove an image map.
 	reqs = f.r.indexer.Enqueue(&v1alpha1.ImageMap{ObjectMeta: metav1.ObjectMeta{Name: "image-c"}})
 	assert.ElementsMatch(t, []reconcile.Request{
-		reconcile.Request{NamespacedName: types.NamespacedName{Name: "b"}},
+		{NamespacedName: types.NamespacedName{Name: "b"}},
 	}, reqs)
 }
 
@@ -158,36 +167,6 @@ func TestGarbageCollectPartial(t *testing.T) {
 	assert.Contains(f.T(), f.kClient.DeletedYaml, "name: infra-kafka-zookeeper")
 }
 
-func TestGarbageCollectAfterErrorDuringApply(t *testing.T) {
-	f := newFixture(t)
-	ka := v1alpha1.KubernetesApply{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "a",
-		},
-		Spec: v1alpha1.KubernetesApplySpec{
-			YAML: fmt.Sprintf("%s\n---\n%s\n", testyaml.SanchoYAML, testyaml.PodDisruptionBudgetYAML),
-		},
-	}
-	f.Create(&ka)
-
-	f.MustReconcile(types.NamespacedName{Name: "a"})
-	assert.Contains(f.T(), f.kClient.Yaml, "name: sancho")
-	assert.Contains(f.T(), f.kClient.Yaml, "name: infra-kafka-zookeeper")
-
-	f.kClient.UpsertError = errors.New("oh no")
-
-	f.MustGet(types.NamespacedName{Name: "a"}, &ka)
-	ka.Spec.YAML = testyaml.SanchoYAML
-	f.Update(&ka)
-
-	// because the apply (upsert) returned an error, no GC should have happened yet
-	f.MustReconcile(types.NamespacedName{Name: "a"})
-	if assert.Empty(t, f.kClient.DeletedYaml) {
-		assert.Contains(f.T(), f.kClient.Yaml, "name: sancho")
-		assert.Contains(f.T(), f.kClient.Yaml, "name: infra-kafka-zookeeper")
-	}
-}
-
 func TestRestartOn(t *testing.T) {
 	f := newFixture(t)
 
@@ -251,6 +230,68 @@ func TestRestartOn(t *testing.T) {
 	f.MustGet(types.NamespacedName{Name: "a"}, &ka)
 	assert.Equal(f.T(), f.kClient.Yaml, "")
 	timecmp.AssertTimeEqual(f.T(), lastApply, ka.Status.LastApplyTime)
+}
+
+func TestDisableByConfigmap(t *testing.T) {
+	f := newFixture(t)
+	ka := v1alpha1.KubernetesApply{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Spec: v1alpha1.KubernetesApplySpec{
+			YAML: testyaml.SanchoYAML,
+			DisableSource: &v1alpha1.DisableSource{
+				ConfigMap: &v1alpha1.ConfigMapDisableSource{
+					Name: "test-disable",
+					Key:  "isDisabled",
+				},
+			},
+		},
+	}
+	f.Create(&ka)
+
+	f.setDisabled(ka.GetObjectMeta().Name, true)
+
+	f.setDisabled(ka.GetObjectMeta().Name, false)
+
+	f.setDisabled(ka.GetObjectMeta().Name, true)
+}
+
+func (f *fixture) requireKaMatchesInApi(name string, matcher func(ka *v1alpha1.KubernetesApply) bool) *v1alpha1.KubernetesApply {
+	ka := v1alpha1.KubernetesApply{}
+
+	require.Eventually(f.T(), func() bool {
+		f.MustGet(types.NamespacedName{Name: name}, &ka)
+		return matcher(&ka)
+	}, timeout, interval)
+
+	return &ka
+}
+
+func (f *fixture) setDisabled(name string, isDisabled bool) {
+	ka := v1alpha1.KubernetesApply{}
+	f.MustGet(types.NamespacedName{Name: name}, &ka)
+
+	require.NotNil(f.T(), ka.Spec.DisableSource)
+	require.NotNil(f.T(), ka.Spec.DisableSource.ConfigMap)
+
+	cm := v1alpha1.ConfigMap{}
+	cmExists := f.Get(types.NamespacedName{Name: ka.Spec.DisableSource.ConfigMap.Name}, &cm)
+	if !cmExists {
+		cm.ObjectMeta.Name = ka.Spec.DisableSource.ConfigMap.Name
+		cm.Data = map[string]string{ka.Spec.DisableSource.ConfigMap.Key: strconv.FormatBool(isDisabled)}
+		f.Client.Create(f.Context(), &cm)
+	} else {
+		cm.Data[ka.Spec.DisableSource.ConfigMap.Key] = strconv.FormatBool(isDisabled)
+		f.Client.Update(f.Context(), &cm)
+	}
+
+	_, err := f.Reconcile(types.NamespacedName{Name: name})
+	require.NoError(f.T(), err)
+
+	f.requireKaMatchesInApi(name, func(ka *v1alpha1.KubernetesApply) bool {
+		return ka.Status.DisableStatus != nil && ka.Status.DisableStatus.Disabled == isDisabled
+	})
 }
 
 type fixture struct {
